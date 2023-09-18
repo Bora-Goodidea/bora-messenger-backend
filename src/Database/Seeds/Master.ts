e4ea56f195a2bc1fce6c74de027f64f51c98f440
psmever <psmever@gmail.com>
2023-09-18 22:57:38 +0900

    build: 기본 프로필 이미지 등록 seed

--- src/Database/Seeds/Master.ts
+++ src/Database/Seeds/Master.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import _ from 'lodash';
+import lodash from 'lodash';
 import MysqlConnect from '@Database/MysqlConnect';
 import { CodeTypeEnum } from '@Entity/Codes';
 import { exit } from 'node:process';
@@ -7,12 +7,13 @@ import Codes from '@Codes';
 
 console.debug(`######################################################################`);
 (async () => {
+    console.debug(`:::::::::::::::::::::::::::::Codes Start::::::::::::::::::::::::::::::`);
     const conn = await MysqlConnect.getConnection();
     const codeData: Array<{ type: CodeTypeEnum; group_id: string; code_id: string; name: string }> = [];
 
-    _.forEach(Codes, (c) => {
+    lodash.forEach(Codes, (c) => {
         codeData.push({ type: `group` as CodeTypeEnum, group_id: c.id, code_id: `${c.id}000`, name: c.name });
-        _.forEach(c.list, (l) => {
+        lodash.forEach(c.list, (l) => {
             codeData.push({
                 type: `code` as CodeTypeEnum,
                 group_id: c.id,
@@ -33,9 +34,23 @@ console.debug(`#################################################################
             `insert into codes (type, group_id, code_id, name, created_at) values ('${code.type}', '${code.group_id}', '${code.code_id}', '${code.name}', now());`,
         );
         if (!result) {
-            console.debug('seed insert error...');
+            console.debug('code insert error...');
             exit();
         }
+    }
+    console.debug(`:::::::::::::::::::::::::::::Codes End::::::::::::::::::::::::::::::`);
+
+    const [result] = await conn.query(`select * from media order by id asc limit 0,1`);
+    if (lodash.isEmpty(result)) {
+        console.debug(`:::::::::::::::::::::::::::::Media Start::::::::::::::::::::::::::::::`);
+        const [inertResult] = await conn.query(
+            `insert into media (type, path, filename, origin_name, size, created_at) values ('image/jpeg', '/profile', 'default_profile.jpg', 'default_profile.jpg', 28350, now());`,
+        );
+        if (!inertResult) {
+            console.debug('media insert error...');
+            exit();
+        }
+        console.debug(`:::::::::::::::::::::::::::::Media End::::::::::::::::::::::::::::::`);
     }
 
     console.debug(`######################################################################`);
