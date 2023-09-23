aae8bab682ec23a100cca62e711fa7aced889a7b
wem519 <wem519@gmail.com>
2023-09-23 15:25:07 +0900

    build: migration profile 추가 & entity 추가

---
+++ src/Database/Entities/Profile.ts
@@ -0,0 +1,29 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToOne, JoinColumn } from 'typeorm';
+import { Users } from './Users';
+import { Media } from './Media';
+
+@Entity()
+export class Profile extends BaseEntity {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({ type: 'int', nullable: false })
+    user_id: number;
+
+    @Column({ type: 'int', nullable: false })
+    profile_image_id: number;
+
+    @Column({ type: `timestamp`, nullable: false })
+    updated_at: string;
+
+    @Column({ type: `timestamp`, nullable: false })
+    created_at: string;
+
+    @OneToOne(() => Users, (u) => u.id, { cascade: true })
+    @JoinColumn({ name: `user_id` })
+    user?: Users;
+
+    @OneToOne(() => Media, (m) => m.id, { cascade: true })
+    @JoinColumn({ name: `profile_image_id` })
+    media?: Media;
+}
